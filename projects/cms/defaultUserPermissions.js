c46e81d02f810831d89cdb70f09a1ea35d946a88
Ayush Goyal <agoyal2001@gmail.com>
2020-07-07 23:52:49 -0400

    Update user permissions

--- projects/cms/defaultUserPermissions.js
+++ projects/cms/defaultUserPermissions.js
@@ -1,8 +1,9 @@
 const defaultUserPermissions = {
-    "rahulrajan.us@gmail.com": 'ADMIN',
-    "rrajan39@gatech.edu": 'ADMIN',
-    "ayush.goyal@gatech.edu": 'ADMIN',
-    "stephanie@hack.gt": 'TECH_TEAM',
+    "rahulrajan.us@gmail.com": "ADMIN",
+    "rrajan39@gatech.edu": "ADMIN",
+    "ayush.goyal@gatech.edu": "ADMIN",
+    "stephanie@hack.gt": "ADMIN",
+    "elton.pinto@hack.gt": "ADMIN"
 }
 
 module.exports = defaultUserPermissions
